b56b552256b320b514730212ce642a2441003f1d
Heather <vesha2002@mail.ru>
2022-05-11 14:39:06 +0300

    lab4

--- lab4/1.js
+++ lab4/1.js
@@ -6,7 +6,34 @@
  * ['мир', 'Рим', 'сирота', 'Ариост', 'мри', 'пва', 'лор', 'авп']; -> [["мир", "Рим", "мри"], ["сирота", "Ариост"], ["пва", "авп"]]
  */
 function getAnagramms(arr) {
-    //code here
+    let change = [];
+    let be =[];
+    let c =[];
+    let removed=[];
+    let myarr=arr;
+    let b=0;
+    let narr= arr.map((x) => x.toUpperCase().split('').sort().join(""));
+ while(narr.length>0){
+      c= narr[0];
+      removed.push(myarr[0]);
+      for (let i=0; i<narr.length;i++){
+      if (c === narr[i]){
+        if (i !=0){
+        narr.splice(i,1);
+        removed.push(myarr[i]);
+        myarr.splice(i,1);
+        console.log(removed);
+        } }
+      }
+      narr.splice(0,1);
+      myarr.splice(0,1);
+      change[b]= removed;
+      b++;
+      removed=[];
+    }
+
+        return change
+
 }
 
 module.exports = getAnagramms;
--- lab4/2.js
+++ lab4/2.js
@@ -7,7 +7,25 @@
  * "тот" -> true
  */
 function isPalindrome(str) {
-    //code here
+    let a=0;
+    let b = true;
+    if (str.length&2===0){
+        a=str.length/2;
+    }
+    else{
+        a=str.length/2-1/2;
+    }
+
+    top:
+    for (let i=0; i<a; i++)
+    {
+        if (str[i].toUpperCase()!=str[str.length-i-1].toUpperCase()){
+            b=false
+            break top;
+        }
+
+    }
+    return b;
 }
 
 module.exports = isPalindrome;
