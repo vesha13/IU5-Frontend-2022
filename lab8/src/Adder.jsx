f572fa70bc6f94b1b00bcb1037c8ff34068ab6be
Heather <vesha2002@mail.ru>
2022-05-30 18:37:07 +0300

    lab8 done

---
+++ lab8/src/Adder.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import './App.css';
+
+function Adder({addTask}) {
+    const[userInput, setUserInput] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        addTask(userInput)
+        setUserInput('')
+    }
+
+    return (
+        <div onSubmit = {handleSubmit} className='Adder'>
+            <input
+            class='input'
+            value = {userInput}
+            type = 'text'
+            onChange = {(e) =>{
+                setUserInput(e.currentTarget.value)}}
+            onKeyDown = {(e) =>{
+                if(e.key === "Enter" && userInput!== ''){
+                    handleSubmit(e)
+                }}}
+            placeholder = "Добавляй сюда свои делишки!"
+            />
+            <button className='Button' onClick={(e) => {if(userInput!== '')handleSubmit(e); else alert('пустая строчка - не совсем дело, впиши что-то')}}><h4>тык</h4></button>
+        </div>
+
+    )
+}
+
+export default Adder
--- lab8/src/App.js
+++ lab8/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import List from './List';
+import Adder from './Adder';
 import './App.css';
 
 function App() {
+    const[goal, SetGoal]= useState([])
+
+    const addTask = (userInput) =>{
+            const newItem = {
+                id: goal.length,
+                task: userInput,
+                completed: false
+
+            }
+            SetGoal([...goal, newItem])
+    }
+
+    const done = (id) => {
+       const cur = goal.splice(id, 1)
+       SetGoal([...goal.filter((todo) => todo !==cur)])
+    }
+
+
+    const Tap = (id) => {
+        SetGoal(goal.map(todo =>
+            {
+              if (todo.id === id) { todo.completed = !todo.completed }
+              return todo;
+          }))
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = "App">
+      <header className = 'head' >
+            <h1>Список дел, чтобы быть молодцом</h1>
       </header>
+      <Adder
+     addTask={addTask}
+      />
+      {goal.map((todo) =>{
+      return (<List 
+        todo = {todo}
+        Tap = {Tap}
+        done = {done}
+        />)
+      })}
     </div>
   );
 }
